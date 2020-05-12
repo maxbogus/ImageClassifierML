efda21774338f63798ba85260c8a77cffd73aacd
Max Boguslavskiy <max.bogus@gmail.com>
2020-05-12 18:15:05 +0300

    wrote draft of command line trainer

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 # Imports here
+import argparse
 import matplotlib.pyplot as plt
 
 import torch
@@ -9,3 +10,128 @@ from torchvision import datasets, transforms, models
 from PIL import Image
 import numpy as np
 from torch.autograd import Variable
+
+# parse args
+parser = argparse.ArgumentParser(description='Parse arguments.')
+parser.add_argument("--model", type=int, default=1)
+parser.add_argument("--learning-rate", type=float, defalut=0.003)
+parser.add_argument("--hidden-units")
+parser.add_argument("--epochs", type=int, default=1)
+parser.add_argument("--use-gpu", type=bool, default=False)
+
+args = parser.parse_args()
+
+models = args.model
+learningRate = args.learningRate
+hiddenUnits = args.hiddenUnits
+epochs = args.epochs
+useGpu = args.useGpu
+
+data_dir = 'flowers'
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+test_dir = data_dir + '/test'
+
+if useGpu:
+   if torch.cuda.is_available() == False:
+       print('GPU is not supported')
+       exit
+
+train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomResizedCrop(224),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([0.485, 0.456, 0.406],
+                                                            [0.229, 0.224, 0.225])])
+
+valid_transforms = transforms.Compose([transforms.Resize(255),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406],
+                                                           [0.229, 0.224, 0.225])])
+
+test_transforms = transforms.Compose([transforms.Resize(255),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406],
+                                                           [0.229, 0.224, 0.225])])
+
+train_data = datasets.ImageFolder(train_dir, transform=train_transforms)
+valid_data = datasets.ImageFolder(valid_dir, transform=valid_transforms)
+test_data = datasets.ImageFolder(test_dir, transform=test_transforms)
+
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+validloader = torch.utils.data.DataLoader(valid_data, batch_size=64)
+testloader = torch.utils.data.DataLoader(test_data, batch_size=64)
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = models.densenet121(pretrained=True)
+model
+
+# Freeze parameters so we don't backprop through them
+for param in model.parameters():
+    param.requires_grad = False
+    
+model.classifier = nn.Sequential(nn.Linear(1024, 256),
+                                 nn.ReLU(),
+                                 nn.Dropout(0.2),
+                                 nn.Linear(256, 102),
+                                 nn.LogSoftmax(dim=1))
+
+criterion = nn.NLLLoss()
+
+# Only train the classifier parameters, feature parameters are frozen
+optimizer = optim.Adam(model.classifier.parameters(), lr=0.003)
+
+model.to(device);
+
+# TODO: Do validation on the test set
+epochs = 1
+steps = 0
+running_loss = 0
+print_every = 5
+for epoch in range(epochs):
+    for inputs, labels in trainloader:
+        steps += 1
+        # Move input and label tensors to the default device
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        optimizer.zero_grad()
+        
+        logps = model.forward(inputs)
+        loss = criterion(logps, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        
+        if steps % print_every == 0:
+            test_loss = 0
+            accuracy = 0
+            model.eval()
+            with torch.no_grad():
+                for inputs, labels in testloader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    logps = model.forward(inputs)
+                    batch_loss = criterion(logps, labels)
+                    
+                    test_loss += batch_loss.item()
+                    
+                    # Calculate accuracy
+                    ps = torch.exp(logps)
+                    top_p, top_class = ps.topk(1, dim=1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+                    
+            print(f"Epoch {epoch+1}/{epochs}.. "
+                  f"Train loss: {running_loss/print_every:.3f}.. "
+                  f"Test loss: {test_loss/len(testloader):.3f}.. "
+                  f"Test accuracy: {accuracy/len(testloader):.3f}")
+            running_loss = 0
+            model.train()
+
+checkpoint = {'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict()}
+
+torch.save(checkpoint, 'checkpoint.pth')

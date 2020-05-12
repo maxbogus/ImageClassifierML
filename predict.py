552f559413fa4751414e4c8f38f25f9d77671d54
Max Boguslavskiy <max.bogus@gmail.com>
2020-05-12 21:43:35 +0300

    fixed typo

--- predict.py
+++ predict.py
@@ -6,8 +6,6 @@ import json
 import sys
 import argparse
 
-import matplotlib.pyplot as plt
-
 import torch
 from torch import nn
 from torch import optim
@@ -21,7 +19,7 @@ import numpy as np
 # parse args
 PARSER = argparse.ArgumentParser(description='Parse arguments.')
 PARSER.add_argument("--image", type=str, default='flowers/valid/102/image_08006.jpg')
-PARSER.add_argument("--checkpoint", type=str, defalut='checkpoint.pth')
+PARSER.add_argument("--checkpoint", type=str, default='checkpoint.pth')
 PARSER.add_argument("--json", type=str, default='cat_to_name.json')
 PARSER.add_argument("--epochs", type=int, default=1)
 PARSER.add_argument("--gpu", type=bool, default=False)
@@ -56,20 +54,23 @@ with open(JSON_FILE, 'r') as f:
     CAT_TO_NAME = json.load(f)
 
 def load_checkpoint(filepath):
+    """
+    Load saved checkpoint
+    """
     checkpoint = torch.load(filepath)
     models.densenet121(pretrained=True)
-    model.classifier = nn.Sequential(
+    MODEL.classifier = nn.Sequential(
         nn.Linear(1024, 256),
         nn.ReLU(),
         nn.Dropout(0.2),
         nn.Linear(256, 102),
         nn.LogSoftmax(dim=1))
-    criterion = nn.NLLLoss()
-    optimizer = optim.Adam(model.classifier.parameters(), lr=0.003)
-    model.load_state_dict(checkpoint['model_state_dict'])
+    #criterion = nn.NLLLoss()
+    optimizer = optim.Adam(MODEL.classifier.parameters(), lr=0.003)
+    MODEL.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
-    return model
+    return MODEL
 
 def process_image(image):
     ''' Scales, crops, and normalizes a PIL image for a PyTorch model,
@@ -120,16 +121,16 @@ def predict(image_path, model, topk=5):
     inputs = Variable(img).to(DEVICE)
     logits = model.forward(inputs)
 
-    APPLIED_SOFTMAX = F.softmax(logits, dim=1)
-    topk = APPLIED_SOFTMAX.cpu().topk(topk)
+    applied_softmax = F.softmax(logits, dim=1)
+    topk = applied_softmax.cpu().topk(topk)
 
     return (e.data.numpy().squeeze().tolist() for e in topk)
 
-model = load_checkpoint(CHECKPOINT)
+MODEL = load_checkpoint(CHECKPOINT)
 
-PROBS, PREDICTED_CLASSES = predict(IMAGE, model)
+PROBS, PREDICTED_CLASSES = predict(IMAGE, MODEL)
 CLASS_NAMES = TRAIN_DATA.classes
 LABELS = [CAT_TO_NAME[CLASS_NAMES[e]] for e in PREDICTED_CLASSES]
-y_pos = np.arange(len(PROBS))
+Y_POS = np.arange(len(PROBS))
 
-print(LABELS, y_pos, CLASS_NAMES, PROBS, PREDICTED_CLASSES)
+print(LABELS, Y_POS, CLASS_NAMES, PROBS, PREDICTED_CLASSES)
